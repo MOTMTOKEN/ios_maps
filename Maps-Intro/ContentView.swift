c4b0846dd65e9b179d3ec9d4ee75c20d39527325
David Svensson <davidsvson@gmail.com>
2022-01-07 09:15:45 +0100

    Initial Commit

---
+++ Maps-Intro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maps-Intro
+//
+//  Created by David Svensson on 2022-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Maps-Intro/Maps_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maps_IntroApp.swift
+//  Maps-Intro
+//
+//  Created by David Svensson on 2022-01-07.
+//
+
+import SwiftUI
+
+@main
+struct Maps_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
